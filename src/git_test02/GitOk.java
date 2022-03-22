b07f7b1a2ba3ba3ef48e07d2e0b530b8ea2a438a
JBEDU000 <JBEDU000@JBEDU00>
2022-03-22 09:42:59 +0900

    v1.1 출력내용수정

--- src/git_test02/GitOk.java
+++ src/git_test02/GitOk.java
@@ -5,6 +5,7 @@ public class GitOk {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("GitOk!!!!!");
+		System.out.print("Nice Upload!!!!!");
 
 	}
 
